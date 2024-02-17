44548ae8717c442a47a297a55541cbe9d009de57
terrabite3 <terrabite3@gmail.com>
2024-02-17 15:11:28 -0500

    Add switch support
    Generate random characters except when all switches are off, then it displays "Hello!"

--- firmware/seg25_puzzle/main.cpp
+++ firmware/seg25_puzzle/main.cpp
@@ -88,28 +88,103 @@ void write_to_display(uint8_t digitIndex, uint32_t segments)
     gpio_put(digit_OE, 0);
 }
 
+const uint switch_SER_OUT = 12;
+const uint switch_SH_LD = 13;
+const uint switch_CLK = 14;
+const uint switch_CLK_INH = 15;
+
+void setup_switch_gpio()
+{
+    gpio_init(switch_SER_OUT);
+    gpio_init(switch_SH_LD);
+    gpio_init(switch_CLK);
+    gpio_init(switch_CLK_INH);
+    
+    gpio_set_dir(switch_SER_OUT, GPIO_IN);
+    gpio_set_dir(switch_SH_LD, GPIO_OUT);
+    gpio_set_dir(switch_CLK, GPIO_OUT);
+    gpio_set_dir(switch_CLK_INH, GPIO_OUT);
+
+    // Disable clock inhibit
+    gpio_put(switch_CLK_INH, 0);
+    
+    // Put shift/load signal in default state
+    gpio_put(switch_SH_LD, 1);
+}
+
+uint16_t read_switch()
+{
+    gpio_put(switch_CLK, 0);
+    
+    // Capture the data
+    gpio_put(switch_SH_LD, 0);
+    gpio_put(switch_SH_LD, 1);
+
+    uint16_t result = 0;
+    for (uint8_t i = 0; i < 16; ++i)
+    {
+        gpio_put(switch_CLK, 1);
+        gpio_put(switch_CLK, 0);
+
+        // Invert, since an up switch produces a 0
+        if (!gpio_get(switch_SER_OUT))
+        {
+            result |= 1 << i;
+        }
+    }
+
+    return result;
+}
+
+uint16_t xorshift_state = 1;
+
+uint16_t xorshift()
+{
+    xorshift_state ^= xorshift_state << 7;
+    xorshift_state ^= xorshift_state >> 9;
+    xorshift_state ^= xorshift_state << 8;
+    return xorshift_state;
+}
+
 int main() 
 {
-
     setup_display_gpio();
-
-    write_to_display(0, 0b100000000000000000000000);
+    setup_switch_gpio();
 
     while (true)
     {
-        write_to_display(0, 0b00110011'00010001'00000000);
-        sleep_ms(1);
-        write_to_display(1, 0b11001111'00000001'00000000);
-        sleep_ms(1);
-        write_to_display(2, 0b00001111'00000000'00000000);
-        sleep_ms(1);
-        write_to_display(3, 0b00001111'00000000'00000000);
-        sleep_ms(1);
-        write_to_display(4, 0b11111111'00000000'00000000);
-        sleep_ms(1);
-        write_to_display(5, 0b00000000'01000100'00000100);
-        sleep_ms(1);
-        write_to_display(6, 0b00000000'00000000'00000000);
-        sleep_ms(1);
+        auto switches = read_switch();
+
+        if (switches == 0)
+        {
+            write_to_display(0, 0b00110011'00010001'00000000);  // H
+            sleep_ms(1);
+            write_to_display(1, 0b11001111'00000001'00000000);  // E
+            sleep_ms(1);
+            write_to_display(2, 0b00001111'00000000'00000000);  // L
+            sleep_ms(1);
+            write_to_display(3, 0b00001111'00000000'00000000);  // L
+            sleep_ms(1);
+            write_to_display(4, 0b11111111'00000000'00000000);  // O
+            sleep_ms(1);
+            write_to_display(5, 0b00000000'01000100'00000100);  // !
+            sleep_ms(1);
+            write_to_display(6, 0b00000000'00000000'00000000);  // nothing
+            sleep_ms(1);
+        }
+        else
+        {
+            // Generate some pseudorandom data seeded from the switch state
+
+            xorshift_state = switches;
+            for (uint8_t digit = 0; digit < 7; ++digit)
+            {
+                uint32_t payload = xorshift() | (xorshift() << 16);
+
+                write_to_display(digit, payload);
+                sleep_ms(1);
+            }
+        }
+
     }
 }
